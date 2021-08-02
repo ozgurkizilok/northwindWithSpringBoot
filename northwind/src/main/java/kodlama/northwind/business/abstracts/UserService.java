78164f258a77e22007556d9c30c34ca997f55f46
OEM <OEM@192.168.1.29>
2021-08-02 13:24:59 +0300

    adding users and validations.

---
+++ northwind/src/main/java/kodlama/northwind/business/abstracts/UserService.java
@@ -0,0 +1,10 @@
+package kodlama.northwind.business.abstracts;
+
+import kodlama.northwind.core.entities.User;
+import kodlama.northwind.core.utilities.results.DataResult;
+import kodlama.northwind.core.utilities.results.Result;
+
+public interface UserService {
+	Result add(User user);
+	DataResult<User> findByProductName(String email);
+}
